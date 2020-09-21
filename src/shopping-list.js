8d3b9789d098ccd8ba829df773ac239c443f2f1f
Nick Jang <nickjang@alumni.upenn.edu>
2020-09-21 12:21:34 -0400

    Add Webpack

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,5 +1,5 @@
-import store from './store.js';
-import item from './item.js';
+import store from './store';
+import $ from 'jquery';
 
 const generateItemElement = function (item) {
   let itemTitle = `<span class="shopping-item shopping-item__checked">${item.name}</span>`;
